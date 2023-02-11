4b49cc590b4c5140899f534d4f0fd13095d4abe0
Towhidul Islam <todevarman@gmail.com>
2023-02-11 17:38:19 +0600

    fab name change

--- resources/views/layout/guest.blade.php
+++ resources/views/layout/guest.blade.php
@@ -6,7 +6,7 @@
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
   <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-  <title>Institute Management System</title>
+  <title>Cash Deposit</title>
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
