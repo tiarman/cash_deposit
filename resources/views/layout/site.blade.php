29c130d6b802bc4c0b5b2ed3f4098350141b5a59
Ti Arman <todevarman@gmail.com>
2023-02-21 22:05:32 +0600

    updated

--- resources/views/layout/site.blade.php
+++ resources/views/layout/site.blade.php
@@ -68,12 +68,17 @@
             <div class="animation-preloader">
                 <div class="spinner"></div>
                 <div class="txt-loading">
-                    <span data-text-preloader="H" class="letters-loading">H</span>
-                    <span data-text-preloader="O" class="letters-loading">O</span>
+                    <span data-text-preloader="H" class="letters-loading">C</span>
+                    <span data-text-preloader="O" class="letters-loading">A</span>
                     <span data-text-preloader="S" class="letters-loading">S</span>
-                    <span data-text-preloader="T" class="letters-loading">T</span>
-                    <span data-text-preloader="I" class="letters-loading">I</span>
-                    <span data-text-preloader="M" class="letters-loading">M</span>
+                    <span data-text-preloader="T" class="letters-loading">H</span>
+                    <span data-text-preloader="I" class="letters-loading">D</span>
+                    <span data-text-preloader="M" class="letters-loading">E</span>
+                    <span data-text-preloader="M" class="letters-loading">P</span>
+                    <span data-text-preloader="M" class="letters-loading">O</span>
+                    <span data-text-preloader="M" class="letters-loading">S</span>
+                    <span data-text-preloader="M" class="letters-loading">I</span>
+                    <span data-text-preloader="M" class="letters-loading">T</span>
                 </div>
             </div>
         </div>
